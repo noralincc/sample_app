6147c5a5deb1a88766c6cd4c671914718a406069
Nora Lin <nora@casecommons.org>
2013-05-12 23:54:23 -0400

    up to 10.1.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,6 +45,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
 
   describe "accessible attributes" do
     it "should not allow access to admin" do
