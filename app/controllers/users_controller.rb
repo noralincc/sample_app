a4acdb03023c045d18d9379399b8d45288cdfba6
Nora Lin <nora@casecommons.org>
2013-06-09 22:00:56 -0400

    up to 11.2.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
@@ -46,7 +47,6 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
   
-
   def destroy
     @user = User.find(params[:id])
     if current_user?(@user) 
@@ -57,8 +57,22 @@ class UsersController < ApplicationController
       flash[:success] = "User destroyed."
       redirect_to users_url
     end  
-
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
 
   private
